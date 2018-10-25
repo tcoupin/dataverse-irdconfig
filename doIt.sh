5824747099b7f33f658706527965da96731f88a7
Thibault Coupin <thibault.coupin@gmail.com>
2018-10-25 14:33:21 +0200

    Custom citation.tsv for IRD

--- doIt.sh
+++ doIt.sh
@@ -15,8 +15,12 @@ read -p "Datacite username:" DATACITE_USERNAME
 read -s -p "Datacite password:"  DATACITE_PASSWORD
 echo
 updateJVMOption doi.baseurlstring https://mds.datacite.org/
-updateJVMOption doi.username $DATACITE_USERNAME
-updateJVMOption doi.password $DATACITE_PASSWORD
+
+if [[ "$DATACITE_USERNAME" != "" ]]
+then
+    updateJVMOption doi.username $DATACITE_USERNAME
+    updateJVMOption doi.password $DATACITE_PASSWORD
+fi
 
 curl -X PUT -d DataCite http://localhost:8080/api/admin/settings/:DoiProvider
 curl -X PUT -d doi http://localhost:8080/api/admin/settings/:Protocol
@@ -24,7 +28,7 @@ curl -X PUT -d 10.5072 http://localhost:8080/api/admin/settings/:Authority
 curl -X PUT -d FK2/ http://localhost:8080/api/admin/settings/:Shoulder
 
 # Metadatablocks
-curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @$(dirname $0)/data/citation.tsv -H "Content-type: text/tab-separated-values"
+curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @$(dirname $0)/data/citation.csv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @$(dirname $0)/data/geospatial.tsv -H "Content-type: text/tab-separated-values"
 
 
