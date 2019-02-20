5545cca6d20457345f82b1d12cf1e47755a31937
Thibault Coupin <thibault.coupin@gmail.com>
2019-02-20 15:44:26 +0100

    custom kind of data, role publisher

--- config/configMe.sh
+++ config/configMe.sh
@@ -28,25 +28,30 @@ curl -X PUT -d 10.5072 http://localhost:8080/api/admin/settings/:Authority
 curl -X PUT -d FK2/ http://localhost:8080/api/admin/settings/:Shoulder
 
 # Metadatablocks
+curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @$(dirname $0)/data/citation-tnu.tsv -H "Content-type: text/tab-separated-values"
 curl http://localhost:8080/api/admin/datasetfield/load -X POST --data-binary @$(dirname $0)/data/geospatial.tsv -H "Content-type: text/tab-separated-values"
 
+# Add role publisher
+curl -H "Content-type:application/json" -d @$(dirname $0)/data/role-publisher.json http://localhost:8080/api/admin/roles/
 
 # Branding
-curl -X PUT -d  "$(dirname $0)/data/homePage.html" http://localhost:8080/api/admin/settings/:HomePageCustomizationFile
+#curl -X PUT -d  "$(dirname $0)/data/homePage.html" http://localhost:8080/api/admin/settings/:HomePageCustomizationFile
 
 mkdir -p /opt/glassfish4/glassfish/domains/domain1/docroot/logos/navbar/
 cp -f $(dirname $0)/data/logo_IRD.png /opt/glassfish4/glassfish/domains/domain1/docroot/logos/navbar/logo.png
 curl -X PUT -d '/logos/navbar/logo.png' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
 
-curl -X PUT -d "$(dirname $0)/data/header.html" http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
+#curl -X PUT -d "$(dirname $0)/data/header.html" http://localhost:8080/api/admin/settings/:HeaderCustomizationFile
 
-curl -X PUT -d 'true' http://localhost:8080/api/admin/settings/:DisableRootDataverseTheme # ne pas afficher le nom, le logo du dataverse root
+#curl -X PUT -d 'true' http://localhost:8080/api/admin/settings/:DisableRootDataverseTheme # ne pas afficher le nom, le logo du dataverse root
 
-curl -X PUT -d "$(dirname $0)/data/footer.html" http://localhost:8080/api/admin/settings/:FooterCustomizationFile
+#curl -X PUT -d "$(dirname $0)/data/footer.html" http://localhost:8080/api/admin/settings/:FooterCustomizationFile
 
 curl -X PUT -d ", Institut de Recherche pour le Développement" http://localhost:8080/api/admin/settings/:FooterCopyright
- curl -X PUT -d https://dvwiki.ird.fr http://localhost:8080/api/admin/settings/:GuidesBaseUrl
- curl -X PUT -d "4.9.4" http://localhost:8080/api/admin/settings/:GuidesVersion
+#curl -X PUT -d https://dvwiki.ird.fr http://localhost:8080/api/admin/settings/:GuidesBaseUrl
+
+VERSION=$(cat $(dirname $0)/../dataverse/pom.xml| grep '<version>' | head -n 1 | sed "s#^[^>]*>##g" | sed 's#<[^<]*$##g' | sed 's#-IRD[0-9]*##g')
+curl -X PUT -d "$VERSION" http://localhost:8080/api/admin/settings/:GuidesVersion
 
 #NavbarAbout URL ?
 #StatusMessageHeader
@@ -54,8 +59,8 @@ curl -X PUT -d "For testing only..." http://localhost:8080/api/admin/settings/:S
 #StatusMessageText
 curl -X PUT -d "This appears in a popup." http://localhost:8080/api/admin/settings/:StatusMessageText
 #:NavbarAboutUrl
-curl -X PUT -d https://dvwiki.ird.fr http://localhost:8080/api/admin/settings/:NavbarAboutUrl
+curl -X PUT -d https://data.ird.fr http://localhost:8080/api/admin/settings/:NavbarAboutUrl
 
- curl -X PUT -d https://dvwiki.ird.fr/doku.php http://localhost:8080/api/admin/settings/:ApplicationPrivacyPolicyUrl
+curl -X PUT -d https://data.ird.fr http://localhost:8080/api/admin/settings/:ApplicationPrivacyPolicyUrl
 
 echo
